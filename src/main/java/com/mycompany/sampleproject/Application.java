d3e3aa7826fb87ccd6165235f8d6095ccff0562a
Carter Rows <116571149+carterrows@users.noreply.github.com>
2023-04-06 10:54:19 -0400

    Update Application.java

--- src/main/java/com/mycompany/sampleproject/Application.java
+++ src/main/java/com/mycompany/sampleproject/Application.java
@@ -10,11 +10,14 @@ import java.util.logging.Logger;
  */
 public class Application {
     
+    // throws file not found exception and parse exception
     public static void main(String[] args) throws FileNotFoundException, ParseException {
         try {
             System.out.println("1420 Project:\n");
+            // call scenario processor class "parse and excecute", with the directory for json file
             ScenarioProcessor.ParseAndExecute("D:\\Desktop\\Windows\\Documents\\NetBeansProjects\\FINAL-PROJECT-ENGG1420\\test.json");
         } catch (org.json.simple.parser.ParseException ex) {
+            // catch parse exception error
             Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
