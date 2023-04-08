40eb4a0fc9648e00794652df620bb879f3a2af05
Abdi-Ahmed-Bare <116572644+Abdi-Ahmed-Bare@users.noreply.github.com>
2023-04-07 21:17:08 -0400

    Update Entry.java

--- src/main/java/com/mycompany/sampleproject/Entry.java
+++ src/main/java/com/mycompany/sampleproject/Entry.java
@@ -4,9 +4,12 @@ package com.mycompany.sampleproject;
  *
  * @author Carter Rows
  */
-// interface for an entry
+// interface for each entry
 public interface Entry {
+    // gets the name of the entry
     String getName();
+    // gets the length of the entry
     long getLength();
+    // checks wether is directory or not
     boolean isDirectory();
 }
